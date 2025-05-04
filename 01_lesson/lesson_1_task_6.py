51d012f12f87f309e3fc172d149f7d20cb70a593
ale27074479 <ale27074479@yandex.ru>
2025-05-04 13:58:36 +0300

    Добавил папку 01_lesson с файлами заданий

---
+++ 01_lesson/lesson_1_task_6.py
@@ -0,0 +1,25 @@
+my_height = 160
+print(my_height)
+
+my_name = "Alex"
+my_name = "Alex Chernyshova"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
